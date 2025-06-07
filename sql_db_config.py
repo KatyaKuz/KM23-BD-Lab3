e9f3f96d9e9e3d185781cfd6be9f32f1acd12f6c
KatyaKuz <92037713+KatyaKuz@users.noreply.github.com>
2025-06-07 16:00:33 +0300

    Update sql_db_config.py

--- sql_db_config.py
+++ sql_db_config.py
@@ -1,4 +1,5 @@
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 username = 'postgres'
 password = 'your_password'
@@ -6,3 +7,4 @@ host = 'localhost'
 database = 'lab3-weather-db'
 
 engine = create_engine(f'postgresql+psycopg2://{username}:{password}@{host}/{database}')
+Session = sessionmaker(bind=engine)

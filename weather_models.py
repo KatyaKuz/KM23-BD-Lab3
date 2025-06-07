c859b62108e75065f510884c8b998bc18b99b00d
KatyaKuz <92037713+KatyaKuz@users.noreply.github.com>
2025-06-07 19:38:03 +0300

    Update weather_models.py

--- weather_models.py
+++ weather_models.py
@@ -1,5 +1,5 @@
-from sqlalchemy.orm import declarative_base
-from sqlalchemy import (Column, Integer, Float, String, Date, Time, Enum )
+from sqlalchemy.orm import declarative_base, relationship
+from sqlalchemy import (Column, Integer, Float, String, Date, Time, Enum, ForeignKey )
 import enum
 
 Base = declarative_base()
@@ -30,9 +30,19 @@ class Weather(Base):
     last_updated = Column(Date) # дата
     sunrise = Column(Time) # час
 
+    wind = relationship("Wind", back_populates="weather", uselist=False)
+
+class Wind(Base):
+    __tablename__ = 'wind'
+    
+    id = Column(Integer, primary_key=True)
+    weather_id = Column(Integer, ForeignKey('weather.id'), nullable=False)  # зовнішній ключ weather_id
+
     wind_degree = Column(Integer)# ціле
     wind_kph = Column(Float)# дробне
     wind_mph = Column(Float)
     wind_direction = Column(Enum(WindDirection))# enum
     wind_gust_mph = Column(Float)
     wind_gust_kph = Column(Float)
+
+    weather = relationship("Weather", back_populates="wind")

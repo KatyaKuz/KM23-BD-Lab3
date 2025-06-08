fe7e325df377f2106fae0b5347751000fa9504fb
KatyaKuz <92037713+KatyaKuz@users.noreply.github.com>
2025-06-08 20:23:25 +0300

    Update sql_db_config.py

--- sql_db_config.py
+++ sql_db_config.py
@@ -1,10 +1,27 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-username = 'postgres'
-password = 'your_password'
 host = 'localhost'
-database = 'lab3-weather-db'
 
-engine = create_engine(f'postgresql+psycopg2://{username}:{password}@{host}/{database}')
-Session = sessionmaker(bind=engine)
+# PostgreSQL
+username_pst = 'postgres'
+password_pst = 'your_password'
+database_pst = 'lab3-weather-db'
+
+pg_engine = create_engine(f'postgresql+psycopg2://{username_pst}:{password_pst}@{host}/{database_pst}')
+PGSession = sessionmaker(bind=pg_engine)
+
+# MySQL
+username_mysql = 'root'
+password_mysql = 'your_password'
+database_mysql = 'lab3_weather_mysql'
+
+mysql_engine = create_engine(f'mysql+pymysql://{username_mysql}:{password_mysql}@{host}:3306/{database_mysql}')
+MySQLSession = sessionmaker(bind=mysql_engine)
+
+DB_TYPE = "postgresql"  # або "mysql" або "postgresql"
+
+if DB_TYPE == "postgresql":
+    Session = PGSession
+elif DB_TYPE == "mysql":
+    Session = MySQLSession
